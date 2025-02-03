98a746666df31a07400edfb7f50eb8bfe154ddbe
nguyenaivi <longduong2412@gmail.com>
2025-02-04 01:18:02 +0700

    Hello Long Duong

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './snippets/**/*.liquid',
+    './layout/*.liquid',
+    './templates/**/*.liquid',
+    './sections/**/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
