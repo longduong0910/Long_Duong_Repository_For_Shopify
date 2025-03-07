34f36dfd7f23b8d0ecdf50bc8fe426e7437279c5
longduong0910 <longduong2412@gmail.com>
2025-03-07 13:49:52 +0700

    Update product json and template

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
     safelist: ['space-x-10']
   },
   theme: {
-    extend: {},
+    extend: { 
+      height: {
+        '94': '22rem'
+      }
+    },
   },
   plugins: [],
 }
